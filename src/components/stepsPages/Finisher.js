8d15163abebd4ff8d9063e61eed31e7f69fe1d3d
RomanAlexeyev <alekseev.roman.1992@mail.ru>
2022-12-13 02:56:36 +0300

    изменил метод закрытия

--- src/components/stepsPages/Finisher.js
+++ src/components/stepsPages/Finisher.js
@@ -4,7 +4,7 @@ export const Finisher = ({masterValue, tg}) => {
     return (
         <>
             <div className="question-title"></div>
-            <button className="button-next" onClick={() => tg.sendData(masterValue)}>
+            <button className="button-next" onClick={() => tg.close()}>
                 <span>Получить эссе</span>
             </button>
         </>
